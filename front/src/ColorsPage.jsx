2db46282a46237abd1cadafeb18559641a06d993
szzphi <leezanyaaa@gmail.com>
2024-12-12 14:39:24 +0200

    css

--- front/src/ColorsPage.jsx
+++ front/src/ColorsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./pages.css";
+import "./colorspages.css";
 
 const ColoursPage = () => {
     const [colors, setColors] = useState([]);
@@ -57,16 +57,8 @@ const ColoursPage = () => {
   <ul>
     {colors.length > 0 ? (
       colors.map((color, index) => (
-        <div
-        key={index}
-        className="color-item"
-        style={{
-          backgroundColor: color.toLowerCase(), // Dynamically set the box background
-          color: ["white",  "cream"].includes(color.toLowerCase()) ? "black" : "white", // Adjust text color for contrast
-        }}
-          onClick={() => handleColorClick(color)} // Navigate on click
-        >
-          <li>{color}</li>
+        <div key={index} className="color-item">
+          <li onClick={() => handleColorClick(color)}>{color}</li>
         </div>
       ))
     ) : (
